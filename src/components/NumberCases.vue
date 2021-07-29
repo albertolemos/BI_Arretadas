72f95786d9dac61c31ee98313199d19936ab889f
albertolemos <albertolemos@gmail.com>
2021-07-28 22:09:57 -0300

    Troca coordenadas para bairro em Alertas/Denúncias

--- src/components/NumberCases.vue
+++ src/components/NumberCases.vue
@@ -117,10 +117,7 @@ export default {
             (this.alerts = response.data.map((el) => {
               return {
                 date: moment(el.date).format("DD-MM-YYYY"),
-                coordinates: {
-                  latitude: el.latitude,
-                  longitude: el.longitude,
-                },
+                district: el.district,
               };
             }))
         )
@@ -134,10 +131,7 @@ export default {
             (this.complaints = response.data.map((el) => {
               return {
                 date: moment(el.date).format("DD-MM-YYYY"),
-                coordinates: {
-                  latitude: el.latitude,
-                  longitude: el.longitude,
-                },
+                district: el.district,
               };
             }))
         )
