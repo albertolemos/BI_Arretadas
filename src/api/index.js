5e73d037d417f90fb4818ef53a0008031eb72e8c
albertolemos <albertolemos@gmail.com>
2021-08-01 01:28:37 -0300

    add baseUrl local

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: "https://arretadas-api.herokuapp.com",
-  headers: {'Content-type': 'application/json'}
+  // baseURL: "https://arretadas-api.herokuapp.com",
+  baseURL: "http://localhost:3000",
+  headers: { 'Content-type': 'application/json' }
 })
