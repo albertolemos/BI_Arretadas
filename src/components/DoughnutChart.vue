eb4bca51047600711eeb3d9b778b8fa0a402176a
Victor Raphael <56162733+victoorraphael@users.noreply.github.com>
2021-09-12 20:24:11 -0300

    feat(doughnut): preenche dados da api e cores dinamicas

--- src/components/DoughnutChart.vue
+++ src/components/DoughnutChart.vue
@@ -3,15 +3,18 @@ import { Doughnut } from "vue-chartjs";
 
 export default {
   extends: Doughnut,
+  props: {
+    dados: Object
+  },
 
   data: () => ({
     chartdata: {
-      labels: ["Heliópolis", "São José", "Boa Vista"],
+      labels: [],
       datasets: [
         {
           label: "Alertas",
-          backgroundColor: ["#94D1BE", "#DAFFEF", "#689689"],
-          data: [15, 5, 9],
+          backgroundColor: [],
+          data: [],
         },
       ],
     },
@@ -20,6 +23,26 @@ export default {
       maintainAspectRatio: false,
     },
   }),
+
+  created() {
+    this.chartdata.labels = Object.keys(this.$props.dados)
+
+    this.chartdata.labels.forEach(l => {
+      let hex = ''
+      let temp = ''
+      for (let index = 0; index < l.length; index++) {
+        temp = Number(l.charCodeAt(index).toString(16)) 
+        if (!isNaN(temp) && hex.length < 6)
+          hex += ''+temp
+      }
+      temp = ''
+      if(hex.length <= 6)
+        this.chartdata.datasets[0].backgroundColor.push(`#${hex}`)
+      hex = ''
+    })
+
+    this.chartdata.datasets[0].data = Object.values(this.$props.dados)
+  },
 
   mounted() {
     this.renderChart(this.chartdata, this.options);
