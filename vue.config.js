42244b24c9ebbf403f7361fe46a04c632763e897
Alberto Lemos <albertolemos@gmail.com>
2021-09-28 00:03:50 -0300

    Fix build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/vue-chart-example/'
-        : '/'
-}
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//         ? '/vue-chart-example/'
+//         : '/'
+// }
