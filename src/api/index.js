f48bb97d2a4ce29e5a3fead174a9b6d6cbae4c27
Alberto Lemos <albertolemos@gmail.com>
2021-10-02 14:47:25 -0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  // baseURL: "https://arretadas-api.herokuapp.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://arretadas-api.herokuapp.com",
+  // baseURL: "http://localhost:3000",
   headers: { 'Content-type': 'application/json' }
 })
