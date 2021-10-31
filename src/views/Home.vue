7065ec0af41968dd65e0432c74b2c6ea25d39124
Alberto Lemos <albertolemos@gmail.com>
2021-10-31 00:57:45 -0300

    Update Home.vue
    Added button logout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <v-app>
-    <Header />
+    <Header></Header>
     <div class="box">
       <h2>Pesquisar</h2>
+      <v-card-text style="height: 100px; position: absolute">
+        <v-fab-transition>
+          <v-btn color="#00d1b2" dark absolute top right fab @click="logout()">
+            <v-icon color="#fff">mdi-logout</v-icon>
+          </v-btn>
+        </v-fab-transition>
+      </v-card-text>
       <NumberCases></NumberCases>
     </div>
     <Footer></Footer>
@@ -15,6 +22,9 @@ import Header from "../components/Header";
 import NumberCases from "../components/NumberCases";
 import Footer from "../components/Footer";
 
+import { mdiLogout } from "@mdi/js";
+import "@mdi/font/css/materialdesignicons.css";
+
 export default {
   name: "app",
   vuetify,
@@ -22,6 +32,18 @@ export default {
     Header,
     NumberCases,
     Footer,
+    mdiLogout,
+    icons: {
+      iconFont: "mdi-logout",
+    },
+  },
+
+  methods: {
+    logout() {
+      sessionStorage.removeItem("userToken");
+      sessionStorage.removeItem("token");
+      this.$router.replace("/login");
+    },
   },
 };
 </script>
@@ -37,5 +59,4 @@ export default {
   font-size: 25px;
   margin-bottom: 1em;
 }
-
 </style>
