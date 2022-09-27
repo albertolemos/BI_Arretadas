caecd9fba9bb213e649e352c509c896cc5911802
albertoribeiro <albertoribeiro@joystreet.com.br>
2022-09-26 22:13:55 -0300

    Adicionando o mapa de calor e deixando ele funcional

--- src/components/NumberCases.vue
+++ src/components/NumberCases.vue
@@ -1,8 +1,11 @@
 <template>
   <v-container>
-    <Form @my-alerts="getAlerts" @my-complaints="getComplaints" @my-clean="cleanLoading"/>
+    <Form
+      @my-alerts="getAlerts"
+      @my-complaints="getComplaints"
+      @my-clean="cleanLoading"
+    />
 
-    
     <div class="container-chart">
       <div class="chart-alerts" v-if="isLoadedAlert && !isEmpty">
         <h2>Por Data (dados em %)</h2>
@@ -48,10 +51,23 @@
             :key="complaintsByDistricts.__ob__.dep.id"
           />
         </div>
-        <div id="map"></div>
+        <!-- <div id="map"></div> -->
       </div>
 
-      <h1 class="mensagem" v-if="isEmpty">Me desculpe, mas não foi localizado nenhum chamado proveniente dessa delimitação de tempo :(</h1>
+      <h1 class="mensagem" v-if="isEmpty">
+        Me desculpe, mas não foi localizado nenhum chamado proveniente dessa
+        delimitação de tempo :(
+      </h1>
+    </div>
+    <div>
+      <vue-google-heatmap
+        :points="points"
+        :width="640"
+        :height="420"
+        :initial-zoom="initialZoom"
+        :lat="lat"
+        :lng="lng"
+      />
     </div>
   </v-container>
 </template>
@@ -59,16 +75,16 @@
 <script>
 import BarChart from "./BarChart.vue";
 import DoughnutChart from "./DoughnutChart.vue";
-import Form from './Form.vue';
-import callHeatMap from '../services/heatMap'
+import Form from "./Form.vue";
+import callHeatMap from "../services/heatMap";
 
 export default {
   name: "numberCases",
-  
+
   components: {
     BarChart,
     DoughnutChart,
-    Form
+    Form,
   },
 
   data() {
@@ -82,49 +98,94 @@ export default {
       complaintsByDates: {},
       complaintsByDistricts: {},
       complaintsByTypes: {},
+      initialZoom: 13,
+      // lat: -7.891580368210672,
+      // lng: -37.12604518424489,
+      lat: -8.892911,
+      lng: -36.49101,
+      points: [
+        { lat: -8.892911, lng: -36.49101 },
+        { lat: -8.892922, lng: -36.49102 },
+        { lat: -8.892933, lng: -36.49103 },
+        { lat: -8.892944, lng: -36.49104 },
+        { lat: -8.892955, lng: -36.49105 },
+        { lat: -8.892966, lng: -36.49106 },
+        { lat: -8.892977, lng: -36.49107 },
+        { lat: -8.892985, lng: -36.49108 },
+        { lat: -8.892995, lng: -36.49109 },
+        // { lat: 37.771, lng: -122.434 },
+        // { lat: 37.772, lng: -122.434 },
+        // { lat: 37.773, lng: -122.434 },
+        // { lat: 37.774, lng: -122.434 },
+        // { lat: 37.77, lng: -122.434 },
+        // { lat: 37.774, lng: -122.434 },
+        // { lat: 37.777, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+        // { lat: 37.779, lng: -122.434 },
+      ],
     };
   },
 
   methods: {
     async getAlerts(date) {
-      this.token = localStorage.getItem('token')
+      this.token = localStorage.getItem("token");
       this.$api.defaults.headers.common[
         "Authorization"
       ] = `Bearer ${this.token}`;
       await this.$api
-      .get(`/alert?init=${date.init}&final=${date.final}`)
-      .then((response) => {
+        .get(`/alert?init=${date.init}&final=${date.final}`)
+        .then((response) => {
           this.alertsByDates = response.data.Date;
           this.alertsByDistricts = response.data.District;
           this.isLoadedAlert = true;
           this.isLoadedComplaint = false;
-          if(Object.values(this.alertsByDates).length == 0 || Object.values(this.alertsByDistricts).length == 0) this.isEmpty = true;
+          if (
+            Object.values(this.alertsByDates).length == 0 ||
+            Object.values(this.alertsByDistricts).length == 0
+          )
+            this.isEmpty = true;
           else this.isEmpty = false;
-      }).catch(() => this.logout())
+        })
+        .catch(() => this.logout());
     },
 
     async getComplaints(date, typeComplaint) {
-      this.token = localStorage.getItem('token')
+      this.token = localStorage.getItem("token");
       const type = typeComplaint == "Todas" ? "all" : typeComplaint;
-      
+
       this.$api.defaults.headers.common[
-      "Authorization"
+        "Authorization"
       ] = `Bearer ${this.token}`;
       await this.$api
-      .get(`/complaint?init=${date.init}&final=${date.final}&type=${type}`)
-      .then((response) => {
+        .get(`/complaint/?init=${date.init}&final=${date.final}&type=${type}`)
+        .then((response) => {
           this.complaintsByDates = response.data.Date;
           this.complaintsByDistricts = response.data.District;
           this.complaintsByTypes = response.data.Type;
           this.isLoadedComplaint = true;
           this.isLoadedAlert = false;
-          if(Object.values(this.complaintsByDates).length == 0 || Object.values(this.complaintsByDistricts).length == 0 || Object.values(this.complaintsByTypes).length == 0) this.isEmpty = true;
+          if (
+            Object.values(this.complaintsByDates).length == 0 ||
+            Object.values(this.complaintsByDistricts).length == 0 ||
+            Object.values(this.complaintsByTypes).length == 0
+          )
+            this.isEmpty = true;
           else this.isEmpty = false;
-          callHeatMap()
-      }).catch(() => this.logout())
+          callHeatMap();
+        })
+        .catch(() => this.logout());
     },
 
-    cleanLoading(){
+    cleanLoading() {
       this.isLoadedAlert = false;
       this.isLoadedComplaint = false;
     },
@@ -133,13 +194,11 @@ export default {
       localStorage.removeItem("token");
       this.$router.replace("/");
     },
-  }
-
+  },
 };
 </script>
 
 <style scoped>
-
 #map {
   width: 500px;
   height: 400px;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import vuetify from './plugins/vuetify';
 import App from './App';
 import api from './api/index'
 import router from './router'
+import VueGoogleHeatmap from 'vue-google-heatmap';
+
+Vue.use(VueGoogleHeatmap, {
+  apiKey: 'AIzaSyDcjVfd-QY09V7TDv27TaM6Hrt1L_CAmns'
+});
 
 Vue.prototype.$api = api
 
