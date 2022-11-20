a535c4211e974ed89edc6cf97d7e38d849d94974
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2022-11-20 17:54:50 -0300

    [FIX] Ajuste na api

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-  // baseURL: "https://arretadas-api-qdkzaivqyq-uc.a.run.app/",
+  baseURL: "https://arretadas-api-qdkzaivqyq-uc.a.run.app/",
   // baseURL: "https://arretadas-api.herokuapp.com",
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
   headers: { 'Content-type': 'application/json' }
 })
