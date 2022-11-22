f212e5dc8c0110910b1effdd530e46229bfa56e2
Alberto Ribeiro de Lemos <albertolemos@gmail.com>
2022-11-21 21:40:33 -0300

    [FIX] Correção da URL base da API

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
-export default axios.create({
-  baseURL: "https://arretadas-api-qdkzaivqyq-uc.a.run.app/",
-  // baseURL: "https://arretadas-api.herokuapp.com",
+export default axios.create({  
+  baseURL: "https://arretadas-api-qdkzaivqyq-ue.a.run.app/",
   // baseURL: "http://localhost:3000",
   headers: { 'Content-type': 'application/json' }
 })
